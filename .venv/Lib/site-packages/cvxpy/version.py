d316d24680d9e5fefe9ef8730e3f09d2161deeb7
wakepon <wkpn@outlook.jp>
2022-06-19 10:16:00 +0900

    ABtest 1st commit

---
+++ .venv/Lib/site-packages/cvxpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CVXPY SETUP.PY
+short_version = '1.2.1'
+version = '1.2.1'
+full_version = '1.2.1'
+git_revision = 'd275ee4'
+commit_count = '0'
+release = True
+if not release:
+    version = full_version
